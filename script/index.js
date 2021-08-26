ed7f9e3ff50f7e4c93752ece84b18d227f312cb9
Manuel Ernst <manuel.ernst@methodpark.de>
2021-08-26 09:12:13 +0200

    add typescript example

--- script/index.js
+++ script/index.js
@@ -1,1 +1,14 @@
-console.log('welcome!')
+import { sum } from './lib/math'
+
+console.log(`
+              _                          _
+__      _____| | ___ ___  _ __ ___   ___| |
+\\ \\ /\\ / / _ \\ |/ __/ _ \\| '_   _ \\ / _ \\ |
+ \\ V  V /  __/ | (_| (_) | | | | | |  __/_|
+  \\_/\\_/ \\___|_|\\___\\___/|_| |_| |_|\\___(_)
+
+`)
+
+const number1 = 42;
+const number2 = 1337;
+console.log(`the sum of ${number1} and ${number2} is ${sum(number1, number2)}`)
---
+++ script/lib/math.ts
@@ -0,0 +1,7 @@
+export function sum(a: number, b: number): number {
+    return a + b;
+}
+
+export function square(a: number): number {
+    return Math.pow(a, 2);
+}
