f0bd477b5891fe0d9d14e32124db003f798a03f3
Frank Blendinger <frank.blendinger@methodpark.de>
2021-08-27 12:21:29 +0200

    Cleanup JS/TS files

--- script/index.js
+++ script/index.js
@@ -1,4 +1,4 @@
-import { sum } from './lib/math'
+import { sum } from './lib/math';
 
 console.log(`
               _                          _
@@ -7,8 +7,8 @@ __      _____| | ___ ___  _ __ ___   ___| |
  \\ V  V /  __/ | (_| (_) | | | | | |  __/_|
   \\_/\\_/ \\___|_|\\___\\___/|_| |_| |_|\\___(_)
 
-`)
+`);
 
 const number1 = 42;
 const number2 = 1337;
-console.log(`the sum of ${number1} and ${number2} is ${sum(number1, number2)}`)
+console.log(`the sum of ${number1} and ${number2} is ${sum(number1, number2)}`);
